d8cd101d6b86f91077ed98674758e3bb3f48ac20
Rrens <yusufrendi313@gmail.com>
2024-03-23 22:06:07 +0700

    Category All

--- src/category/category.repository.js
+++ src/category/category.repository.js
@@ -1,11 +1,14 @@
 const prisma = require('../db')
 
 const findCategories = async () => {
-    return await prisma.category.findMany({
-        include: {
-            product: true
-        }
-    })
+    const categoriesWithItemCount = await prisma.$queryRaw`
+        SELECT c.name, c.id, CONVERT(COUNT(p.id), CHAR) as itemCount 
+        FROM categories as c 
+        JOIN products as p ON c.id = p.category_id 
+        GROUP BY c.id
+    `;
+
+    return categoriesWithItemCount;
 }
 
 module.exports = {
